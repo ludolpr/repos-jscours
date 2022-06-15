5c89e7bcfbb69df0a81e2628f35dd499ce8f839e
ludolpr <ludolpr@gmail.com>
2022-06-15 16:54:15 +0200

    soir 16h54

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 // Liste de categorie
-const personnelList = ["Brandon","Christian", "Joris", "Eddy", "Dan", "Axel", "Théo", "Ludo"];
+const personnelList = [
+  "Brandon",
+  "Christian",
+  "Joris",
+  "Eddy",
+  "Dan",
+  "Axel",
+  "Théo",
+  "Ludo",
+];
 
 // DOM : accés rapide à la categorie ID
 
@@ -8,7 +17,6 @@ const personnelForm = document.getElementById("personnel");
 // Liste du personnel paramètre
 
 personnelList.forEach((objet) => {
-
   // console.log(`<option>${objet}</option>`);
 
   // option ci dessous serf a nommée la balise creer
@@ -18,64 +26,164 @@ personnelList.forEach((objet) => {
   perso.value = objet;
 
   personnelForm.add(perso);
-})
+});
 function addNewProduct() {
   console.log("Une personne à été ajouté");
 
+  // Récupère les valeurs du formulaire
+  const personnel = document.getElementById("personnel").value;
+  const poste = document.getElementById("poste").value;
+  const anciens = document.getElementById("anciens").value;
+  const heureTravaille = document.getElementById("heureTravaille").value;
 
-// Récupère les valeurs du formulaire
-const personnel = document.getElementById("personnel").value;
-const poste = document.getElementById("poste").value;
-const anciens = document.getElementById("anciens").value;
-const heureTravaille = document.getElementById("heureTravaille").value;
+  console.log(poste, anciens, heureTravaille, personnel);
 
-console.log(poste, anciens, heureTravaille,personnel);
+  const id = new Date().valueOf();
 
- const id = new Date().valueOf();
- 
- // Nouveau membres
+  // Nouveau membres
 
- const membres = {
-   id: id,
-   poste: poste,
-   Nom: personnel,
-   Ancienneté: anciens,
-   heureTravaille: heureTravaille 
- }
- console.log("Nouveau membre:", membres)
+  const membres = {
+    id: id,
+    poste: poste,
+    Nom: personnel,
+    Ancienneté: anciens,
+    heureTravaille: heureTravaille,
+  };
+  console.log("Nouveau membre:", membres);
 
- let membresList = localStorage.getItem('membres') ? JSON.parse(localStorage.getItem('membres')) : [];
- membresList.push(membres);
+  let membresList = localStorage.getItem("membres")
+    ? JSON.parse(localStorage.getItem("membres"))
+    : [];
+  membresList.push(membres);
 
+  localStorage.setItem("membres", membresList);
 
- localStorage.setItem("membres", membresList);
-
- // La valeur doit etre convertit en string, on ne peux pas stocker d'object ni d'array.
- localStorage.setItem("membres", JSON.stringify(membresList));
-
+  // La valeur doit etre convertit en string, on ne peux pas stocker d'object ni d'array.
+  localStorage.setItem("membres", JSON.stringify(membresList));
 }
 
 // Récupère le contenu du localStorage
 function getMembre() {
-  return localStorage.getItem('membres') ? JSON.parse(localStorage.getItem('membres')) : [];
+  return localStorage.getItem("membres")
+    ? JSON.parse(localStorage.getItem("membres"))
+    : [];
 }
 // console.log(getMembre());
 
 // Affiche les produits dans le tableau
-(
-function showMembresInTable() {
+(function showMembresInTable() {
   const membres = getMembre();
-  const table = document.getElementById('tableContent');
-  
-  membres.forEach((item) => {
+  const table = document.getElementById("tableContent");
+  if (membres.length) {
+    const message = document.getElementById("message");
+    message.style.display = "none";
+  }
+
+  // console.log(membres);
+  membres.forEach((item, index) => {
+    // console.log(item);
     let row = table.insertRow();
     let id = row.insertCell(0);
-    let name = row.insertCell(1);
-    
-    id.innerHTML = 1;
-    name.innerHTML = item.name
-  })
-  
-}
-)()
+    let poste = row.insertCell(1);
+    let anciens = row.insertCell(2);
+    let heureTravaille = row.insertCell(3);
+    let action = row.insertCell(4);
+    let nom = row.insertCell(2); // ajoute une colone nom
 
+    //   name : value
+
+    id.innerHTML = index + 1;
+    poste.innerHTML = `<img src='./images/${item.poste.toLowerCase()}.png' />${
+      item.poste
+    }`;
+    nom.innerHTML = item.Nom; // ajoute dans la colone nom la valeur de la propriéte Nom
+    anciens.innerHTML = `${item.Ancienneté} ans`;
+    heureTravaille.innerHTML = `${item.heureTravaille} heures`;
+    action.innerHTML = `
+    <a onclick="showEditMembres(${item.id})"><i class="fas fa-edit iconEdit"></i></a>
+    <a onclick="deleteMembres(${item.id})"><i class="fas fa-trash-alt iconDelete"></i></a>
+    `;
+    // site d'imagesci-dessous
+    // https://pixabay.com/fr/vectors/escalade-escalade-de-rocher-corde-99111/
+  });
+})();
+
+// Affiche le formulaire en modal pour éditer
+function showEditMembres(id) {
+  const membres = getMembre();
+  //const product = products.find( function (element) {return element.id == id})
+  const membresEdit = membres.find((element) => element.id == id);
+  console.log("product :", membresEdit, membresEdit.Nom);
+
+  let modalEditMembres = new bootstrap.Modal(
+    document.getElementById("modalEditMembres")
+  );
+  modalEditMembres.show();
+  // poste
+  let editPoste = document.getElementById("editName");
+  editPoste.value = membresEdit.poste;
+
+  // ancienneté
+  let editAnciens = document.getElementById("editPrice");
+  editAnciens.value = membresEdit.Ancienneté;
+
+  //  heureTravaille
+  let editHeureTravaille = document.getElementById("editQuantity");
+  editHeureTravaille.value = membresEdit.heureTravaille;
+
+  //  Nom
+  const editNom = document.getElementById("editNom");
+  editNom.innerHTML = null;
+
+  personnelList.forEach((item) => {
+    let option = document.createElement("option");
+
+    if (membresEdit.Nom == item) {
+      option.selected = true;
+    }
+    option.text = item;
+    option.value = item;
+    editNom.add(option);
+  });
+
+  //  id
+  let editId = document.getElementById("editId");
+  editId.value = membresEdit.id;
+}
+
+function updateMembres() {
+  let editNom = getMembre();
+  const membresId = document.getElementById("editId").value;
+
+  // Récupère le produit pour l'éditer
+  let membresAdd = editNom.find(function (element) {
+    return element.id == membresId;
+  });
+  // Récupère les valeurs du formulaire
+  let poste = document.getElementById("editName").value;
+  let personnel = document.getElementById("editNom").value;
+  let anciens = document.getElementById("editPrice").value;
+  let heureTravaille = document.getElementById("editQuantity").value;
+
+  console.log(membresAdd);
+  membresAdd.Nom = personnel;
+  membresAdd.poste = poste;
+  membresAdd.Ancienneté = anciens;
+  membresAdd.heureTravaille = heureTravaille;
+
+  localStorage.setItem("membres", JSON.stringify(editNom));
+}
+
+function deleteMembres(id) {
+  let personnelList = getMembre();
+  const removeMembres = personnelList.findIndex((element) => element.id == id);
+  console.log(removeMembres);
+
+  // Supprime un élément de la liste des produits
+  personnelList.splice(removeMembres, 1);
+
+  // Mets à jour la liste des produits
+  localStorage.setItem("membres", JSON.stringify(personnelList));
+  location.reload();
+
+}
--- app2.js
+++ app2.js
@@ -1,17 +1,56 @@
-// Categories list
-const categoriesList = ['Vegetable', 'Meat', 'Fish', 'Fruit'];
+// Produits Base de donnée statique
+const categoryList = ["Kekra", "Damso", "Laylow", "Jazzy Bazz", "Nekfeu"];
 
-// DOM : access to the ID category element quickly
-const category = document.getElementById('category');
+const rappeur = document.getElementById('category');
 
-
-categoriesList.forEach((item) => {
-  // 1 // console.log(item);
-
-  // 2 // console.log(`<option>${item}</option>`);
-  
+categoryList.forEach((element) => {
   let option = document.createElement("option");
-  option.text = item;
-  option.value = item;
-  category.add(option);
-})
+  option.text = element;
+  option.value = element;
+  rappeur.add(option);
+}
+)
+
+function addNewProduct() {
+  // Récupère les valeurs du formulaire
+  const name = document.getElementById('name').value;
+  const category = document.getElementById('category').value;
+  const price = document.getElementById('price').value;
+  const quantity = document.getElementById('quantity').value;
+
+  // Génère un id pour le produit
+  const id = new Date().valueOf();
+
+  // Nouveau produit
+  const product = {
+    id: id,
+    name: name,
+    category: category,
+    price: price,
+    quantity: quantity
+  }
+  console.log("new product :", product)
+
+
+  // Condition Terniaire
+  let productsList = localStorage.getItem('products') ? JSON.parse(localStorage.getItem('products')) : [];
+  productsList.push(product);
+  
+  // La valeur doit etre convertit en string, on ne peux pas stocker d'object ni d'array.
+  localStorage.setItem("products", JSON.stringify(productsList));
+}
+
+// Récupère le contenu du localStorage
+function getProducts() {
+  return localStorage.getItem('products')? JSON.parse(localStorage.getItem('products')) : [];
+}
+
+// Affiche les produits dans le tableau
+function showProductsInTable() {
+  const products = getProducts(); // Faut il enlever le s de products
+  const table = document.getElementById("tableContent");
+
+  products.forEach((item) => {
+    console.log("items :", item);
+  })
+}
